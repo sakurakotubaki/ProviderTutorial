b6af4adb72b924dd3248ac07d55d5748594f68bb
Jboy Hashimoto <73347485+sakurakotubaki@users.noreply.github.com>
2023-01-31 00:10:45 +0900

    update タイポを修正

--- lib/model/FIrebaseProvider.dart
+++ lib/model/FIrebaseProvider.dart
@@ -22,7 +22,7 @@ class FirebaseProvider extends ChangeNotifier {
   Future<void> updatePost(dynamic document, String postC) async {
     await docRef.doc(document.id).update({
       'post': postC,
-      'createdAt': Timestamp.fromDate(now),
+      'updatedAt': Timestamp.fromDate(now),
     });
     notifyListeners();
   }
